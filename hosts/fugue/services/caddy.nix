45402d1b3a7a05639edb9da97a412f3a100533db
Froup See <froupC@outlook.com>
2024-05-04 23:19:35 +0800

    enable caddy config for fugue

--- hosts/fugue/services/caddy.nix
+++ hosts/fugue/services/caddy.nix
@@ -1,14 +1,16 @@
+{ pkgs, ...}: 
 {
   nix.settings.sandbox = false;
-  # services.caddy = {
-  #   enable = false;
-  #   globalConfig = ''
-  #     debug
-  #     auto_https disable_certs
-  #     servers {
-  #       trusted_proxies cloudflare
-  #       protocols h2 h3
-  #     }
-  #   '';
-  # };
+  services.caddy = {
+    enable = true;
+    package = pkgs.customized_caddy;
+    globalConfig = ''
+      debug
+      auto_https disable_certs
+      servers {
+        trusted_proxies cloudflare
+        protocols h2 h3
+      }
+    '';
+  };
 }
