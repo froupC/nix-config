6ead965dc3c50319efeeb9a4e727f8d53d9349c9
Froup See <froupC@outlook.com>
2024-05-04 18:21:05 +0800

    change service clash2sfa port for fugue

--- hosts/fugue/services/clash2sfa.nix
+++ hosts/fugue/services/clash2sfa.nix
@@ -1,7 +1,7 @@
 {
   virtualisation.oci-containers.containers.clash2sfa = {
     image = "ghcr.io/xmdhs/clash2sfa:v0.13.1";
-    ports = ["127.0.0.1:8080:5149"];
+    ports = ["127.0.0.1:5149:8080"];
     volumes = [
       "clash2sfa:/server/db"
     ];
