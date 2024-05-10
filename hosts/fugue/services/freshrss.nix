698a7756318d1df499f310cd97c0c85d9c525aeb
Froup See <froupC@outlook.com>
2024-05-10 12:46:57 +0800

    fix freshrss for fugue

--- hosts/fugue/services/freshrss.nix
+++ hosts/fugue/services/freshrss.nix
@@ -24,32 +24,34 @@
     ];
 
     config = { config, pkgs, lib, ... }: {
+
       services.freshrss = {
         enable = true;
         baseUrl = "https://read.froup.cc";
         dataDir = "/var/lib/freshrss";
         defaultUser = "froup";
         passwordFile = "/run/secrets/freshrss";
+        # virtualHost = null;
       };
 
       system.stateVersion = "23.11";
 
-      config.nixpkgs.pkgs = pkgs;
-
-      networking.useHostResolvConf = true;
+      networking = {
+        firewall.enable = false;
+        useHostResolvConf = true;
+      };
     };
   };
 
   services.caddy.virtualHosts."read.froup.cc" = {
     extraConfig = ''
-      reverse_proxy http://127.0.0.1:7349 {
+      reverse_proxy http://172.18.0.1:7349 {
         header_up Host {host}
         header_up X-Real-IP {remote}
         header_up X-Forwarded-Proto {scheme}
         header_up X-Forwarded-Host {host}
         header_up X-Forwarded-For {remote}
         header_up X-Forwarded-Ssl {on}
-        header_up X-Forwarded-Prefix "/freshrss/"
       }
       tls /etc/ssl/certs/cloudflare.pem /etc/ssl/certs/cloudflare.key
     '';
