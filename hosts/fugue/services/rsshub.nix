4fbdd500cb555595b90c7a5911c0bcb10cbbce41
Froup See <froupC@outlook.com>
2024-05-11 00:54:35 +0800

    add rsshub (deployed via docker compose)

---
+++ hosts/fugue/services/rsshub.nix
@@ -0,0 +1,16 @@
+{
+  services.caddy.virtualHosts."rsshub.froup.cc" = {
+    extraConfig = ''
+      reverse_proxy http://127.0.0.1:55233 {
+        header_up Host {host}
+        header_up Connection ""
+        header_up X-Real-IP {remote}
+        header_up X-Forwarded-Proto {scheme}
+        header_up X-Forwarded-Host {host}
+        header_up X-Forwarded-For {remote}
+        header_up X-Forwarded-Ssl {on}
+      }
+      tls /etc/ssl/certs/cloudflare.pem /etc/ssl/certs/cloudflare.key
+    '';
+  };
+}
